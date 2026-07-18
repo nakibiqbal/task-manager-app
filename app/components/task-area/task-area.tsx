7b594c598bd78b13062ee51e41cdbd61992be81c
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2026-07-18 12:40:06 +0600

    Added TaskArea component with SearchInput and reset button

---
+++ app/components/task-area/task-area.tsx
@@ -0,0 +1,35 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import SearchInput from "./search-input";
+import { Button } from "@/components/ui/button";
+import { IoCloseSharp } from "react-icons/io5";
+
+export default function TaskArea() {
+  return (
+    <div className="px-7 mb-6">
+      <Card>
+        <CardHeader>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-2">
+              <SearchInput />
+              {/* status drop down */}
+              {/* priority drop down */}
+              <Button variant={"ghost"} className="h-10">
+                <span>Reset</span>
+                <IoCloseSharp />
+              </Button>
+            </div>
+
+            <div>{/* dropdown view coloumn */}OK</div>
+          </div>
+        </CardHeader>
+        <CardContent>{/* table */}</CardContent>
+        <CardFooter>{/* pagination */}</CardFooter>
+      </Card>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/nav-bar";
 import StatCards from "./components/stats-cards";
+import TaskArea from "./components/task-area/task-area";
 
 export default function Home() {
   return (
     <div className={`poppins min-h-screen bg-slate-50 dark:bg-black`}>
       <NavBar />
       <StatCards />
+      <TaskArea />
     </div>
   );
 }
